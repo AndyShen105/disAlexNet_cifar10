c943a57a2f4305724c175bf8c86092f6a8c42e4a
AndyShen <shenhang105@gmail.com>
2017-11-23 20:03:02 +0800

    clean code

--- bin/ps.sh
+++ bin/ps.sh
@@ -6,32 +6,22 @@
 # $5 is the tensorflow port
 # $6 is the empoch
 # $7 batch size
-# ps.sh run in ssd42
+# ps.sh run in b1g72
 
 get_ps_conf(){
     ps=""
-    for(( i=42; i > 42-$1; i-- ))
+    for(( i=72; i > 72-$1; i-- ))
     do
-        if [ $i -lt 10 ]
-        then
-            ps="$ps,ssd0$i:"$2
-        else
-            ps="$ps,ssd$i:"$2
-        fi
+        ps="$ps,b1g$i:"$2
     done
         ps=${ps:1}
 };
 
 get_worker_conf(){
     worker=""
-    for(( i=42-$1; i > 42-$1-$2; i-- ))
+    for(( i=72-$1; i > 72-$1-$2; i-- ))
     do
-        if [ $i -lt 10 ]
-        then
-            worker="$worker,ssd0$i:"$3
-        else
-            worker="$worker,ssd$i:"$3
-        fi
+        worker="$worker,b1g$i:"$3
     done
     worker=${worker:1}
 };
@@ -44,35 +34,31 @@ do
 done
 
 echo "release port occpied!"
-kill_cluster_pid.sh 6 42 $5
+kill_cluster_pid.sh 37 72 $5
 
 get_ps_conf $1 $5
 echo $ps
 get_worker_conf $1 $2 $5
 echo $worker
 
-for(( i=42; i>42-$1-$2; i-- ))
+for(( i=72; i>72-$1-$2; i-- ))
 do
 {
-    if [ $i == 42 ]
+    if [ $i == 72 ]
     then
         python /root/code/disCNN_cifar/disCNN_cifar10.py $ps $worker --job_name=ps --task_index=0
     else
 	ip=""
         if [ $i -lt 10 ]
         then
-            ip="ssd0"$i
-        else
-            ip="ssd"$i
-        fi
-	#ssh ssd$i "source activate tensorflow"
-        n=`expr 42 - $1`
+            ip="b1g"$i
+        n=`expr 72 - $1`
         if [ $i -gt $n ]
         then
-            index=`expr 42 - $i`
+            index=`expr 72 - $i`
             ssh $ip python /root/code/disCNN_cifar/disCNN_cifar10.py $ps $worker --job_name=ps --task_index=$index >> temp$index
         else
-            index=`expr 42 - $1 - $i`
+            index=`expr 72 - $1 - $i`
 	    if [ $index != 0 ]
 	    then
 		sleep 0.5
@@ -97,7 +83,7 @@ do
     done	
     if [ $flag == $2 ]
     then
-    	kill_cluster_pid.sh 16 42 $5
+    	kill_cluster_pid.sh 37 72 $5
 	break
     fi
 done 
--- cifar10.py
+++ cifar10.py
@@ -1,15 +1,5 @@
 #-*-coding:UTF-8-*-
 """Builds the CIFAR-10 network.
-Summary of available functions:
- # Compute input images and labels for training. If you would like to run
- # evaluations, use inputs() instead.
- inputs, labels = distorted_inputs()
- # Compute inference on the model inputs to make a prediction.
- predictions = inference(inputs)
- # Compute the total loss of the prediction with respect to the labels.
- loss = loss(predictions, labels)
- # Create a graph to run one step of training with respect to the loss.
- train_op = train(loss, global_step)
 """
 from __future__ import absolute_import
 from __future__ import division
@@ -27,7 +17,7 @@ import tensorflow as tf
 import cifar10_input
 
  
-data_dir = "hdfs://ssd02:8020/user/root/train_data/cifar-10-batches-bin/"
+data_dir = "hdfs://b10g37:8020/user/root/train_data/cifar-10-batches-bin/"
 use_fp16 = False
 
 #parameters initialize
@@ -48,7 +38,7 @@ def get_optimizer(optimizer, learning_rate):
     elif optimizer == "Adam":
 	return  tf.train.AdamOptimizer(learning_rate)
     elif optimizer == "Momentum":
-	return  tf.train.MomentumOptimizer(learning_rate)
+	return  tf.train.MomentumOptimizer(learning_rate, 0.9)
     elif optimizer == "RMSProp":
 	return  tf.train.RMSProp(learning_rate)
 
@@ -172,7 +162,7 @@ def inputs(eval_data):
   return images, labels
 
 
-def inference(images, batch_size):
+def inference(images, batch_size, , variable_partition_num):
   """Build the CIFAR-10 model.
   Args:
     images: Images returned from distorted_inputs() or inputs().
@@ -190,7 +180,7 @@ def inference(images, batch_size):
                                          shape=[5, 5, 3, 64],
                                          stddev=5e-2,
                                          wd=0.0,
-					 1)
+					                    1)
     conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
     biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0),1)
     pre_activation = tf.nn.bias_add(conv, biases)
@@ -229,7 +219,7 @@ def inference(images, batch_size):
     reshape = tf.reshape(pool2, [batch_size, -1])
     dim = reshape.get_shape()[1].value
     weights = _variable_with_weight_decay('weights', shape=[dim, 384],
-                                          stddev=0.04, wd=0.004,20)
+                                          stddev=0.04, wd=0.004, variable_partition_num)
     biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
     local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
     _activation_summary(local3)
@@ -237,7 +227,7 @@ def inference(images, batch_size):
   # local4
   with tf.variable_scope('local4') as scope:
     weights = _variable_with_weight_decay('weights', shape=[384, 192],
-                                          stddev=0.04, wd=0.004, 20)
+                                          stddev=0.04, wd=0.004, variable_partition_num)
     biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1), 1)
     local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
     _activation_summary(local4)
@@ -255,131 +245,3 @@ def inference(images, batch_size):
     _activation_summary(softmax_linear)
 
   return softmax_linear
-
-
-def loss(logits, labels):
-  """Add L2Loss to all the trainable variables.
-  Add summary for "Loss" and "Loss/avg".
-  Args:
-    logits: Logits from inference().
-    labels: Labels from distorted_inputs or inputs(). 1-D tensor
-            of shape [batch_size]
-  Returns:
-    Loss tensor of type float.
-  """
-  # Calculate the average cross entropy loss across the batch.
-  labels = tf.cast(labels, tf.int64)
-  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-      labels=labels, logits=logits, name='cross_entropy_per_example')
-  cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
-  tf.add_to_collection('losses', cross_entropy_mean)
-
-  # The total loss is defined as the cross entropy loss plus all of the weight
-  # decay terms (L2 loss).
-  return tf.add_n(tf.get_collection('losses'), name='total_loss')
-
-
-def _add_loss_summaries(total_loss):
-  """Add summaries for losses in CIFAR-10 model.
-  Generates moving average for all losses and associated summaries for
-  visualizing the performance of the network.
-  Args:
-    total_loss: Total loss from loss().
-  Returns:
-    loss_averages_op: op for generating moving averages of losses.
-  """
-  # Compute the moving average of all individual losses and the total loss.
-  loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
-  losses = tf.get_collection('losses')
-  loss_averages_op = loss_averages.apply(losses + [total_loss])
-
-  # Attach a scalar summary to all individual losses and the total loss; do the
-  # same for the averaged version of the losses.
-  for l in losses + [total_loss]:
-    # Name each loss as '(raw)' and name the moving average version of the loss
-    # as the original loss name.
-    tf.summary.scalar(l.op.name + ' (raw)', l)
-    tf.summary.scalar(l.op.name, loss_averages.average(l))
-
-  return loss_averages_op
-
-def _add_loss_summaries(total_loss):
-  """Add summaries for losses in CIFAR-10 model.
-  Generates moving average for all losses and associated summaries for
-  visualizing the performance of the network.
-  Args:
-    total_loss: Total loss from loss().
-  Returns:
-    loss_averages_op: op for generating moving averages of losses.
-  """
-  # Compute the moving average of all individual losses and the total loss.
-  loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
-  losses = tf.get_collection('losses')
-  loss_averages_op = loss_averages.apply(losses + [total_loss])
-
-  # Attach a scalar summary to all individual losses and the total loss; do the
-  # same for the averaged version of the losses.
-  for l in losses + [total_loss]:
-    # Name each loss as '(raw)' and name the moving average version of the loss
-    # as the original loss name.
-    tf.summary.scalar(l.op.name + ' (raw)', l)
-    tf.summary.scalar(l.op.name, loss_averages.average(l))
-
-  return loss_averages_op
-
-
-def train(total_loss, global_step):
-  """Train CIFAR-10 model.
-  Create an optimizer and apply to all trainable variables. Add moving
-  average for all trainable variables.
-  Args:
-    total_loss: Total loss from loss().
-    global_step: Integer Variable counting the number of training steps
-      processed.
-  Returns:
-    train_op: op for training.
-  """
-  # Variables that affect learning rate.
-  num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / batch_size
-  decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
-
-  # Decay the learning rate exponentially based on the number of steps.
-  lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
-                                  global_step,
-                                  decay_steps,
-                                  LEARNING_RATE_DECAY_FACTOR,
-                                  staircase=True)
-  tf.summary.scalar('learning_rate', lr)
-
-  # Generate moving averages of all losses and associated summaries.
-  loss_averages_op = _add_loss_summaries(total_loss)
-
-  # Compute gradients.
-  with tf.control_dependencies([loss_averages_op]):
-    opt = tf.train.GradientDescentOptimizer(lr)
-    grads = opt.compute_gradients(total_loss)
-  #train_op = opt.minimize(loss, global_step=global_step)
-  
-  # Apply gradients.
-  apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
-
-  # Add histograms for trainable variables.
-  for var in tf.trainable_variables():
-    tf.summary.histogram(var.op.name, var)
-
-  # Add histograms for gradients.
-  for grad, var in grads:
-    if grad is not None:
-      tf.summary.histogram(var.op.name + '/gradients', grad)
-
-  # Track the moving averages of all trainable variables.
-  variable_averages = tf.train.ExponentialMovingAverage(
-      MOVING_AVERAGE_DECAY, global_step)
-  variables_averages_op = variable_averages.apply(tf.trainable_variables())
-
-  with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
-    train_op = tf.no_op(name='train')
-  
-  return train_op
-
-
